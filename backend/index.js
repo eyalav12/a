d6b002cc70aa9b8822f74719681997001b291ca2
Eyal Avraham <eyalav12@gmail.com>
2024-04-03 19:14:20 +0300

    env variable

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,6 @@ const router=new express.Router();
 const mongoose=require('mongoose');
 const movieRouter=require('./routers/movie');
 const bookRouter=require('./routers/book');
-const Book=require('./models/book');
-const Movie = require('./models/movie');
 const cors = require('cors');
 
 const app=express();
@@ -19,16 +17,16 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(movieRouter);
 app.use(bookRouter);
+require('dotenv').config();
 
-
-mongoose.connect("mongodb+srv://eyalav12:poli12@backenddb.cm3bqvg.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDb")
+mongoose.connect(process.env.MONGODB_URL)
 .then(()=>{
     console.log("connected to db");
     app.listen(8000,()=>{
         console.log("serever is running on port 8000");
     });
-}).catch(()=>{
-    console.log("failed");
+}).catch((e)=>{
+    console.log("failed",e);
 });
 
 
